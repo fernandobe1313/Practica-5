f177e2600bcf296bbc0e0fce6e318c74ce71c1a2
fernandobe1313 <ferdabega5@gmail.com>
2026-06-10 19:05:30 -0600

    practica 7 commit Perfil modificado

--- miapp2/App.js
+++ miapp2/App.js
@@ -6,36 +6,60 @@ import { Saludo2 } from './components/Saludo2';
 import { Perfil } from './components/Perfil';
 
 /* Zona2: Main - hogar de los componentes*/
+
+
 export default function App() {
   return (
     <View style={styles.container}>
 
+      <Perfil nombre="Fernando Daniel Bello García" carrera="Ingeniería en Sistemas Computacionales" materia="Programación Móvil" cuatri="9°" />
 
-      <Text> ---------------------------------------- Componente Propio Nativos --------------------------------------  </Text>
+      <Text> ------------------------------------------------------------------------------------------------------------------------------</Text>
+      <Text> ------------------------------------------------------------------------------------------------------------------------------</Text>
 
+      <Perfil
+        nombre="Fernando Daniel Bello García"
+        carrera="Desarrollo de Software"
+        materia="Programación para Dispositivos Móviles"
+        cuatri="7°"
+      />
 
-      <Text>Repaso de componentes </Text>
-      <Perfil></Perfil>
-
-
-      <Image source={require('./assets/wave.png')} />
-      <Text>Hola mundo React Native</Text>
-
-      <Text> ---------------------------------------- Componente Propio Simple --------------------------------------  </Text>
-
-
-      <Saludo></Saludo>
-
-
-      <Text> ---------------------------------------- Componente Propio Compuesto--------------------------------------  </Text>
-
-
-      <Saludo2></Saludo2>
 
       <StatusBar style="auto" />
+
     </View>
   );
 }
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+
+//       <Text> ---------------------------------------- Componente Propio Nativos --------------------------------------  </Text>
+
+//       <Text>Repaso de componentes </Text>
+//       <Perfil></Perfil>
+
+
+//       <Image source={require('./assets/wave.png')} />
+//       <Text>Hola mundo React Native</Text>
+
+//       <Text> ---------------------------------------- Componente Propio Simple --------------------------------------  </Text>
+
+
+//       <Saludo></Saludo>
+
+
+//       <Text> ---------------------------------------- Componente Propio Compuesto--------------------------------------  </Text>
+
+
+//       <Saludo2></Saludo2>
+
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
 /* Zona3: Estilos y posicionamiento */
 const styles = StyleSheet.create({

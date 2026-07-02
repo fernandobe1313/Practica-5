9c08bb37069fed04064b882521df0125f78efe8c
fernandobe1313 <ferdabega5@gmail.com>
2026-07-01 22:37:22 -0600

    Practica No.16: ActivityIndicator, KeyboardAvoidingView

--- miapp2/screens/ActivityIndicatorScreen.js
+++ miapp2/screens/ActivityIndicatorScreen.js
@@ -1,30 +1,137 @@
-/* Zona1: importaciones de componentes de archivos*/
+/*Zona 1: Importaciones de componentes y archivos */
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    View,
+    KeyboardAvoidingView,
+    TextInput,
+    Text,
+    Platform,
+    TouchableWithoutFeedback,
+    Button,
+    Keyboard,
+    ActivityIndicator,
+    StyleSheet,
+} from 'react-native';
 
-
-/* Zona2: Main - hogar de los componentes*/
-
-
+/*Zona 2: Main - Hogar de los componentes */
 export default function ActivityIndicatorScreen() {
+    const [cargando, setCargando] = useState(false);
+
+    const handleSubmit = () => {
+        Keyboard.dismiss();
+        setCargando(true);
+        setTimeout(() => {
+            setCargando(false);
+        }, 2000);
+    };
+
     return (
-        <View style={styles.container}>
-
-            <Text> Aqui va la practica de Ana </Text>
-            <StatusBar style="auto" />
-
-        </View>
+        <KeyboardAvoidingView
+            style={styles.screen}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}
+            enabled={true}
+        >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Header</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Username"
+                    />
+                    {cargando && (
+                        <ActivityIndicator
+                            animating={true}
+                            size="large"
+                            color="green"
+                            style={styles.spinner}
+                        />
+                    )}
+                    <View style={styles.buttonWrapper}>
+                        <Button title="Submit" onPress={handleSubmit} />
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 }
 
-
-/* Zona3: Estilos y posicionamiento */
+/*Zona 3: Estilos y Posicionamiento */
 const styles = StyleSheet.create({
-    container: {
+    screen: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-        flexDirection: 'column'
-    }
+    },
+
+    container: {
+        padding: 24,
+        flex: 1,
+        justifyContent: 'space-around',
+    },
+
+    header: {
+        fontSize: 36,
+        marginBottom: 48,
+    },
+
+    input: {
+        height: 40,
+        borderColor: '#000000',
+        borderBottomWidth: 1,
+        marginBottom: 36,
+    },
+
+    spinner: {
+        marginVertical: 12,
+    },
+
+    buttonWrapper: {
+        backgroundColor: 'white',
+        marginTop: 12,
+    },
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /* Zona1: importaciones de componentes de archivos*/
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+
+// /* Zona2: Main - hogar de los componentes*/
+
+
+// export default function ActivityIndicatorScreen() {
+//     return (
+//         <View style={styles.container}>
+
+//             <Text> Aqui va la practica de Ana </Text>
+//             <StatusBar style="auto" />
+
+//         </View>
+//     );
+// }
+
+
+// /* Zona3: Estilos y posicionamiento */
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'flex-start',
+//         justifyContent: 'center',
+//         flexDirection: 'column'
+//     }
+// });

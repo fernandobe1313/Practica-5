674b67ded0432cfe335e3b1aff93153bcd16359f
fernandobe1313 <ferdabega5@gmail.com>
2026-07-01 18:07:25 -0600

    Practica No.15:  ImageBackgroung & SlapshScreen

--- miapp2/screens/ImageBackgroundScreen.js
+++ miapp2/screens/ImageBackgroundScreen.js
@@ -1,30 +1,120 @@
-/* Zona1: importaciones de componentes de archivos*/
+/* Zona 1: Importanciones de componentes y archivos */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Button, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
-
-/* Zona2: Main - hogar de los componentes*/
-
-
+/* Zona 2: Main - Hogar de los componentes */
 export default function ImageBackgroundScreen() {
+    const [fondo, setFondo] = useState(
+        require('../assets/fondo.jpeg'));
+
+    const [splash, setSplash] = useState(true);
+
+    useEffect(() => { setTimeout(() => { setSplash(false); }, 3000); }, []);
+
+    if (splash) {
+        return (
+            <View style={styles.splashContainer}>
+                <Image source={require('../assets/wave.png')} resizeMode="contain" style={styles.logo} />
+
+                <Text style={styles.carga}> Cargando...</Text>
+
+                <StatusBar style="auto" />
+            </View>
+        );
+    }
+
+    const cambiarFondo = () => {
+        setFondo(
+            fondo === require('../assets/fondo.jpeg')
+                ? require('../assets/fondo2.jpeg')
+                : require('../assets/fondo.jpeg')
+        );
+    };
+
     return (
-        <View style={styles.container}>
-
-            <Text> Aqui va la practica de Maria Ines </Text>
+        <ImageBackground
+            source={fondo}
+            style={styles.fondo}
+            resizeMode="contain"
+        >
+            <View style={styles.centro}>
+                <Text style={styles.texto}>Bienvenido</Text>
+                <Button title="Cambiar Fondo" onPress={cambiarFondo} />
+            </View>
             <StatusBar style="auto" />
-
-        </View>
+        </ImageBackground>
     );
 }
 
-
-/* Zona3: Estilos y posicionamiento */
+/* Zona3: Estilos y Posicionamiento */
 const styles = StyleSheet.create({
-    container: {
+    fondo: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'flex-start',
+        width: '100%',
+    },
+    centro: {
+        flex: 1,
+        alignItems: 'center',
         justifyContent: 'center',
-        flexDirection: 'column'
-    }
+    },
+    texto: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    splashContainer: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#1E88E5',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    carga: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: 'white',
+        marginTop: 15,
+    },
+    logo: {
+        width: 150,
+        height: 150,
+    },
 });
+
+
+
+
+
+
+
+// /* Zona1: importaciones de componentes de archivos*/
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+
+// /* Zona2: Main - hogar de los componentes*/
+
+
+// export default function ImageBackgroundScreen() {
+//     return (
+//         <View style={styles.container}>
+
+//             <Text> Aqui va la practica de Maria Ines </Text>
+//             <StatusBar style="auto" />
+
+//         </View>
+//     );
+// }
+
+
+// /* Zona3: Estilos y posicionamiento */
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'flex-start',
+//         justifyContent: 'center',
+//         flexDirection: 'column'
+//     }
+// });
